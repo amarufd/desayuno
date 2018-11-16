6d1a42c297b8e7ba170aee2bc5e8b124b14c9a0d
amarufd <amarud@gmail.com>
2018-11-16 00:46:37 -0300

    se completa el desarrollo de las funciones del desayuno

--- acciones.R
+++ acciones.R
@@ -1,43 +1,64 @@
 calentar <- function(x){
-  if(x=="pan"){
+  if(x=="cafe"){
+    return("cafe caliente")
+  }else if(x=="pan cortado"){
     return("pan tostado")
   }else{
-    return("se calento "+x)
+    return(paste("No sé que hacer con ",x))
   }
 }
 
 preparar <- function(x,y){
-  if(x=="palta"){
-    return("palta molida")
-  }else if(x=="pan tostado" && y=="palta molida"){
+  if(x=="naranjas cortadas"){
+    return("jugo de naranjas")
+  }else if((x=="masa de panqueques" && y=="sartén caliente")||(y=="masa de panqueques" && x=="sartén caliente")){
+    return("panqueques")
+  }else if((x=="harina" && y=="huevos")||(y=="harina" && x=="huevos")){
+    return("masa de panqueques")
+  }else if((x=="pan tostado" && y=="palta molida")||(y=="pan tostado" && x=="palta molida")){
     return("pan tostado con palta")
   }else{
-    return("No sé que hacer con "+x+" e "+y)  
+    return("faltan elementos")  
   }
-  
 }
 
 rellenar <- function(x){
-  print(x)
-  return(x)
-}
-
-servir <- function(x){
-  print(x)
-  return(x)
+  if(x=="panqueques"){
+    return("panqueques con manjar")
+  }else{
+    return(paste("No sé que hacer con ",x)) 
+  }
 }
 
 moler <- function(x){
-  print(x)
-  return(x)
+  if(x=="palta cortada"){
+    return("palta molida")
+  }else{
+    return(paste("No sé que hacer con ",x))
+  }
 }
 
 cortar <- function(x){
-  print(x)
-  return(x)
+  if(x=="pan"){
+    return("pan cortado")
+  }else if(x=="naranjas"){
+    return("naranjas cortadas")
+  }else if(x=="palta"){
+    return("palta cortada")
+  }else{
+    return(paste("No sé que hacer con ",x))
+  }
 }
 
 calentarSarten <- function(){
-  print("calentando")
-  return("calentando")
+  return("sartén caliente")
+}
+
+servir <- function(x,y,z,j){
+  if((x=="pan tostado con palta" && y=="panqueques con manjar" && z=="jugo de naranjas" && j=="cafe caliente")){
+    return("mmmmm... niami... el desayuno está maravilloso")
+  }else{
+    return("iiiuuuu... esto no está listo")
+  }
+  
 }
