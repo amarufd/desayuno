be6e70853841680a4859afa9d075c1ae93768099
amarufd <amarud@gmail.com>
2018-11-15 23:16:29 -0300

    se agregan todas las funciones

--- acciones.R
+++ acciones.R
@@ -1,6 +1,31 @@
-tostar <- function(x){
- print(x)
- return(x)
+calentar <- function(x){
+  print(x)
+  return(x)
+}
+
+preparar <- function(x){
+  print(x)
+  return(x)
+}
+
+rellenar <- function(x){
+  print(x)
+  return(x)
+}
+
+servir <- function(x){
+  print(x)
+  return(x)
+}
+
+moler <- function(x){
+  print(x)
+  return(x)
+}
+
+cortar <- function(x){
+  print(x)
+  return(x)
 }
 
 calentarSarten <- function(){
